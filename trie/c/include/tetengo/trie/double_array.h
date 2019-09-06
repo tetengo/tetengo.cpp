6b9fb6e96971c494c9cd4128808ecfffe199ff2a
kaoru <kaorut@mc.pikara.ne.jp>
2019-09-07 03:22:37 +0900

    Create tetengo_trie_doublearray_create/destroy #2

---
+++ trie/c/include/tetengo/trie/double_array.h
@@ -0,0 +1,37 @@
+/*! \file
+    \brief A double array.
+
+    Copyright (C) 2019 kaorut
+*/
+
+#if !defined(TETENGO_TRIE_DOUBLEARRAY_H)
+#define TETENGO_TRIE_DOUBLEARRAY_H
+#if defined(__cplusplus)
+extern "C"
+{
+#endif
+
+    /*!
+        \brief A double array.
+    */
+    struct tetengo_trie_doublearray;
+
+    /*!
+        \brief Creates a double array.
+
+        \return A pointer to a double array.
+    */
+    tetengo_trie_doublearray* tetengo_trie_doublearray_create();
+
+    /*!
+        \brief Destroys a double array.
+
+        \param A pointer to a double array.
+    */
+    void tetengo_trie_doublearray_destroy(tetengo_trie_doublearray* p_double_array);
+
+
+#if defined(__cplusplus)
+}
+#endif
+#endif
