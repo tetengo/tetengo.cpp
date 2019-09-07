e8072f1af96210f2494f45bb7e03df9294ca2af6
kaoru <kaorut@mc.pikara.ne.jp>
2019-09-07 17:37:06 +0900

    Create test_tetengo::trie::double_array #2

---
+++ trie/test/src/master.cpp
@@ -0,0 +1,8 @@
+/*!
+    \brief The master test suite.
+
+    Copyright (C) 2019 kaorut
+ */
+
+#define BOOST_TEST_MODULE tetengo-trie.test
+#include <boost/test/unit_test.hpp>
---
+++ trie/test/src/test.tetengo.trie.double_array.cpp
@@ -0,0 +1,37 @@
+/*!
+    \brief A double array.
+
+    Copyright (C) 2019 kaorut
+ */
+
+#include <boost/scope_exit.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <tetengo/trie/double_array.h>
+#include <tetengo/trie/double_array.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_tetengo)
+BOOST_AUTO_TEST_SUITE(trie)
+BOOST_AUTO_TEST_SUITE(double_array)
+
+
+BOOST_AUTO_TEST_CASE(construction)
+{
+    BOOST_TEST_PASSPOINT();
+
+    {
+        const tetengo::trie::double_array double_array_{};
+    }
+    {
+        tetengo_trie_doublearray* const p_double_array = tetengo_trie_doublearray_create();
+        BOOST_SCOPE_EXIT(p_double_array)
+        {
+            tetengo_trie_doublearray_destroy(p_double_array);
+        } BOOST_SCOPE_EXIT_END;
+    }
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
