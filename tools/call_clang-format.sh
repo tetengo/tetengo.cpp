907bafd4e81167e74adcf33a2e6ac607b2743f9c
kaoru <kaorut@mc.pikara.ne.jp>
2019-09-16 12:58:41 +0900

    Implement call-clang-format.sh #6

--- tools/call_clang-format.sh
+++ tools/call_clang-format.sh
@@ -3,5 +3,17 @@
 # Copyright (C) 2019 kaoru
 
 FILEPATH=$1
+FILEPATH_TMP=${FILEPATH}.tmp
 
+if test -z ${CLANGFORMAT};
+then
+    CLANGFORMAT="clang-format"
+fi
+if test -z ${UNIS2DOS};
+then
+    UNIX2DOS="unix2dos"
+fi
 
+${CLANGFORMAT} ${FILEPATH} > ${FILEPATH_TMP}
+${UNIX2DOS} -q ${FILEPATH_TMP}
+mv -f ${FILEPATH_TMP} ${FILEPATH}
