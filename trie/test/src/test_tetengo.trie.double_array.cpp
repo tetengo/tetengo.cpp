55e8eba81392b77cc27c68b11bc44abdc7fcc4bd
kaoru <kaorut@mc.pikara.ne.jp>
2019-09-22 15:22:22 +0900

    Implement tetengo::trie::double_array::make_base_check(), base_of() and check_of() #2

--- trie/test/src/test_tetengo.trie.double_array.cpp
+++ trie/test/src/test_tetengo.trie.double_array.cpp
@@ -4,6 +4,8 @@
     Copyright (C) 2019 kaoru
  */
 
+#include <cstdint>
+
 #include <boost/preprocessor.hpp>
 #include <boost/scope_exit.hpp>
 #include <boost/test/unit_test.hpp>
@@ -18,6 +20,89 @@ BOOST_AUTO_TEST_SUITE(test_tetengo)
 BOOST_AUTO_TEST_SUITE(trie)
 BOOST_AUTO_TEST_SUITE(double_array)
 
+
+BOOST_AUTO_TEST_CASE(make_base_check)
+{
+    BOOST_TEST_PASSPOINT();
+
+    {
+        const std::int32_t base = 42;
+        const std::int8_t  check = 24;
+
+        const std::uint32_t base_check = tetengo::trie::double_array::make_base_check(base, check);
+
+        BOOST_TEST(base_check == 0x00002A18U);
+    }
+    {
+        // TODO: C style API
+    }
+
+    {
+        const std::int32_t base = -42;
+        const std::int8_t  check = -24;
+
+        const std::uint32_t base_check = tetengo::trie::double_array::make_base_check(base, check);
+
+        BOOST_TEST(base_check == 0xFFFFD6E8U);
+    }
+    {
+        // TODO: C style API
+    }
+}
+
+BOOST_AUTO_TEST_CASE(base_of)
+{
+    BOOST_TEST_PASSPOINT();
+
+    {
+        const std::uint32_t base_check = 0x00002A18;
+
+        const std::int32_t base = tetengo::trie::double_array::base_of(base_check);
+
+        BOOST_TEST(base == 42);
+    }
+    {
+        // TODO: C style API
+    }
+
+    {
+        const std::uint32_t base_check = 0xFFFFD6E8;
+
+        const std::int32_t base = tetengo::trie::double_array::base_of(base_check);
+
+        BOOST_TEST(base == -42);
+    }
+    {
+        // TODO: C style API
+    }
+}
+
+BOOST_AUTO_TEST_CASE(check_of)
+{
+    BOOST_TEST_PASSPOINT();
+
+    {
+        const std::uint32_t base_check = 0x00002A18;
+
+        const std::int32_t check = tetengo::trie::double_array::check_of(base_check);
+
+        BOOST_TEST(check == 24);
+    }
+    {
+        // TODO: C style API
+    }
+
+    {
+        const std::uint32_t base_check = 0xFFFFD6E8;
+
+        const std::int32_t check = tetengo::trie::double_array::check_of(base_check);
+
+        BOOST_TEST(check == -24);
+    }
+    {
+        // TODO: C style API
+    }
+}
 
 BOOST_AUTO_TEST_CASE(construction)
 {
