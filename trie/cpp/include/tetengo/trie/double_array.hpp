65086095f6885ca392661d6a7437f7132934736a
kaoru <kaorut@mc.pikara.ne.jp>
2019-09-22 22:40:57 +0900

    Revert "Implement tetengo::trie::double_array::make_base_check(), base_of() and check_of() #2"
    This reverts commit 55e8eba81392b77cc27c68b11bc44abdc7fcc4bd.

--- trie/cpp/include/tetengo/trie/double_array.hpp
+++ trie/cpp/include/tetengo/trie/double_array.hpp
@@ -7,8 +7,6 @@
 #if !defined(TETENGO_TRIE_DOUBLEARRAY_HPP)
 #define TETENGO_TRIE_DOUBLEARRAY_HPP
 
-#include <cstdint>
-
 
 namespace tetengo::trie
 {
@@ -18,35 +16,6 @@ namespace tetengo::trie
     class double_array
     {
     public:
-        // static functions
-
-        /*!
-            \brief Make a base-check value.
-
-            \param base  A base value.
-            \param check A check value.
-        */
-        static std::uint32_t make_base_check(std::int32_t base, std::int8_t check);
-
-        /*!
-            \brief Returns the base value from a base-check value.
-
-            \param base_check A base-check value.
-
-            \return The base value.
-        */
-        static std::int32_t base_of(std::uint32_t base_check);
-
-        /*!
-            \brief Returns the check value from a base-check value.
-
-            \param base_check A base-check value.
-
-            \return The check value.
-        */
-        static std::int8_t check_of(std::uint32_t base_check);
-
-
         // constructors and destructor
 
         /*!
--- trie/cpp/src/tetengo.trie.double_array.cpp
+++ trie/cpp/src/tetengo.trie.double_array.cpp
@@ -4,34 +4,12 @@
     Copyright (C) 2019 kaoru
 */
 
-#include <cassert>
-#include <cstdint>
 
 #include <tetengo/trie/double_array.hpp>
 
 
 namespace tetengo::trie
 {
-    std::uint32_t double_array::make_base_check(const std::int32_t base, const std::int8_t check)
-    {
-        assert(-8388608 <= base && base < 8388608);
-
-        const std::uint32_t ubase = static_cast<std::uint32_t>(base);
-        const std::uint8_t  ucheck = static_cast<std::uint8_t>(check);
-
-        return (ubase << 8) | ucheck;
-    }
-
-    std::int32_t double_array::base_of(const std::uint32_t base_check)
-    {
-        return static_cast<std::int32_t>(base_check) >> 8;
-    }
-
-    std::int8_t double_array::check_of(const std::uint32_t base_check)
-    {
-        return static_cast<std::int8_t>(base_check & 0xFF);
-    }
-
     double_array::double_array() {}
 
 
