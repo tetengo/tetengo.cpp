eb36f5cee62e1b3f4da75000b3f81276fe7f1e7f
kaoru <kaorut@mc.pikara.ne.jp>
2019-09-29 21:31:05 +0900

    Implement tetengo::trie::storage #2

--- trie/cpp/include/tetengo/trie/storage.hpp
+++ trie/cpp/include/tetengo/trie/storage.hpp
@@ -7,9 +7,98 @@
 #if !defined(TETENGO_TRIE_STORAGE_HPP)
 #define TETENGO_TRIE_STORAGE_HPP
 
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+
+#include <boost/core/noncopyable.hpp>
+
 
 namespace tetengo::trie
 {
+    /*!
+        \brief A storage.
+    */
+    class storage : private boost::noncopyable
+    {
+    public:
+        /*!
+            \brief Creates a storage.
+        */
+        storage() = default;
+
+        /*!
+            \brief Destroys the storage.
+        */
+        virtual ~storage() = default;
+
+
+        // functions
+
+        /*!
+            \brief Returns the base value.
+
+            \param index An index.
+
+            \return The base value.
+        */
+        std::int32_t base_at(std::size_t index) const;
+
+        /*!
+            \brief Sets a base value.
+
+            \param index An index.
+            \param value A value.
+        */
+        void set_base_at(std::size_t index, std::int32_t value);
+
+        /*!
+            \brief Returns the check value.
+
+            \param index An index.
+
+            \return The check value.
+        */
+        std::uint8_t check_at(std::size_t index) const;
+
+        /*!
+            \brief Sets a check value.
+
+            \param index An index.
+            \param value A value.
+        */
+        void set_check_at(std::size_t index, std::uint8_t value);
+
+        /*!
+            \brief Returns the size of this storage.
+
+            \return The size.
+        */
+        std::size_t size() const;
+
+        /*!
+            \brief Clones the storage.
+        */
+        std::unique_ptr<storage> clone() const;
+
+
+    private:
+        // virtual functions
+
+        virtual std::int32_t base_at_impl(std::size_t index) const = 0;
+
+        virtual void set_base_at_impl(std::size_t index, std::int32_t value) = 0;
+
+        virtual std::uint8_t check_at_impl(std::size_t index) const = 0;
+
+        virtual void set_check_at_impl(std::size_t index, std::uint8_t value) = 0;
+
+        virtual std::size_t size_impl() const = 0;
+
+        virtual std::unique_ptr<storage> clone_impl() const = 0;
+    };
+
+
 }
 
 
--- trie/cpp/src/tetengo.trie.storage.cpp
+++ trie/cpp/src/tetengo.trie.storage.cpp
@@ -4,16 +4,44 @@
     Copyright (C) 2019 kaoru
 */
 
+#include <cstddef>
+#include <cstdint>
+#include <memory>
+
 #include <tetengo/trie/storage.hpp>
 
 
 namespace tetengo::trie
 {
-    /*!
-        \brief A storage.
-    */
-    class storage
-    {};
+    std::int32_t storage::base_at(const std::size_t index) const
+    {
+        return base_at_impl(index);
+    }
+
+    void storage::set_base_at(const std::size_t index, const std::int32_t value)
+    {
+        set_base_at_impl(index, value);
+    }
+
+    std::uint8_t storage::check_at(const std::size_t index) const
+    {
+        return check_at_impl(index);
+    }
+
+    void storage::set_check_at(const std::size_t index, const std::uint8_t value)
+    {
+        set_check_at_impl(index, value);
+    }
+
+    std::size_t storage::size() const
+    {
+        return size_impl();
+    }
+
+    std::unique_ptr<storage> storage::clone() const
+    {
+        return clone_impl();
+    }
 
 
 }
