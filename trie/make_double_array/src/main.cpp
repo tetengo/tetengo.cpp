7f7f0d5f594c7c0b9ad1702f915b434f288146f1
kaoru <kaorut@mc.pikara.ne.jp>
2019-10-03 01:52:39 +0900

    Implement make_double_array #9

--- trie/make_double_array/src/main.cpp
+++ trie/make_double_array/src/main.cpp
@@ -4,9 +4,41 @@
     Copyright (C) 2019 kaoru
  */
 
+#include <algorithm>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
+#include <memory>
 #include <string>
+#include <string_view>
+#include <utility>
+#include <vector>
+
+#include <tetengo/trie/double_array.hpp>
+
+
+namespace
+{
+    std::unique_ptr<std::vector<std::pair<std::string, int>>> load_input(const std::filesystem::path& path)
+    {
+        auto p_input = std::make_unique<std::vector<std::pair<std::string, int>>>();
+
+        std::ifstream input_stream{ path.c_str() };
+        for (auto i = 0; input_stream.good(); ++i)
+        {
+            std::string line;
+            std::getline(input_stream, line);
+
+            const auto split_position = std::find(line.begin(), line.end(), '\t');
+            std::string key{ line.begin(), split_position };
+            p_input->emplace_back(std::move(key), i);
+        }
+
+        return p_input;
+    }
+
+
+}
 
 
 int main(const int argc, char** const argv)
@@ -17,16 +49,9 @@ int main(const int argc, char** const argv)
         return 0;
     }
 
-    {
-        std::ifstream input_stream{ argv[1] };
-        while (input_stream.good())
-        {
-            std::string line;
-            std::getline(input_stream, line);
+    const auto p_input = load_input(argv[1]);
 
-            std::cout << line << std::endl;
-        }
-    }
+    tetengo::trie::double_array double_array_{ p_input->begin(), p_input->end() };
 
     return 0;
 }
