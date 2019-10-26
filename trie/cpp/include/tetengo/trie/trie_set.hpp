5b6f0dddc651fe351be1dacc533e28723de3ce33
kaoru <kaorut@mc.pikara.ne.jp>
2019-10-26 22:22:53 +0900

    Implement trie_set #11

--- trie/cpp/include/tetengo/trie/trie_set.hpp
+++ trie/cpp/include/tetengo/trie/trie_set.hpp
@@ -7,14 +7,45 @@
 #if !defined(TETENGO_TRIE_TRIESET_HPP)
 #define TETENGO_TRIE_TRIESET_HPP
 
+#include <cstddef>
+
 
 namespace tetengo::trie
 {
     /*!
         \brief An enumerator.
+
+        \tparam Key     A key type.
+        \tparam Storage A storage type.
     */
-    class enumerator
-    {};
+    template <typename Key, typename Storage>
+    class trie_set
+    {
+    public:
+        // types
+
+        using key_type = Key;
+
+        using value_type = Key;
+
+        using storage_type = Storage;
+
+        using pointer = value_type*;
+
+        using const_pointer = const value_type*;
+
+        using reference = value_type&;
+
+        using const_reference = const value_type&;
+
+        using size_type = std::size_t;
+
+        using difference_type = std::ptrdiff_t;
+
+        using iterator = pointer; // TODO Create a class iterator.
+
+        using const_iterator = const_pointer; // TODO Create a class iterator.
+    };
 
 
 }
