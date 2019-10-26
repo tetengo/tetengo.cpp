37c906858e35d989d6f600e122bc678b4da42579
kaoru <kaorut@mc.pikara.ne.jp>
2019-10-26 22:11:29 +0900

    Create tetengo::trie::trie_set #11

---
+++ trie/cpp/include/tetengo/trie/trie_set.hpp
@@ -0,0 +1,23 @@
+/*! \file
+    \brief A trie set.
+
+    Copyright (C) 2019 kaoru
+*/
+
+#if !defined(TETENGO_TRIE_TRIESET_HPP)
+#define TETENGO_TRIE_TRIESET_HPP
+
+
+namespace tetengo::trie
+{
+    /*!
+        \brief An enumerator.
+    */
+    class enumerator
+    {};
+
+
+}
+
+
+#endif
---
+++ trie/test/src/test_tetengo.trie.trie_set.cpp
@@ -0,0 +1,26 @@
+/*!
+    \brief A trie set.
+
+    Copyright (C) 2019 kaoru
+ */
+
+#include <boost/preprocessor.hpp>
+#include <boost/test/unit_test.hpp>
+
+#include <tetengo/trie/trie_set.hpp>
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo)
+BOOST_AUTO_TEST_SUITE(trie)
+BOOST_AUTO_TEST_SUITE(trie_set)
+
+
+BOOST_AUTO_TEST_CASE(construction)
+{
+    BOOST_TEST_PASSPOINT();
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
