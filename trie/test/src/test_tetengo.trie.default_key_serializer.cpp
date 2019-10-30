d3ab211e7c3910df9c59fb44737900784ea7e32f
kaoru <kaorut@mc.pikara.ne.jp>
2019-10-31 00:42:43 +0900

    Implement default_key_serializer #11

--- trie/test/src/test_tetengo.trie.default_key_serializer.cpp
+++ trie/test/src/test_tetengo.trie.default_key_serializer.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <string>
+#include <vector>
 
 #include <boost/preprocessor.hpp>
 #include <boost/test/unit_test.hpp>
@@ -12,11 +13,33 @@
 #include <tetengo/trie/default_key_serializer.hpp>
 
 
+namespace tetengo::trie
+{
+    template <typename Key>
+    class default_key_serializer;
+
+    template <typename Char>
+    class default_key_serializer<std::vector<Char>>
+    {
+    public:
+        // functions
+
+        std::string operator()(const std::vector<Char>& key) const
+        {
+            return std::string{ key.begin(), key.end() };
+        }
+    };
+
+
+}
+
+
 namespace
 {
-    class my_class
-    {};
-
+    constexpr char to_c(const unsigned char uc)
+    {
+        return static_cast<char>(uc);
+    }
 
 }
 
@@ -45,13 +68,21 @@ BOOST_AUTO_TEST_CASE(operator_paren)
         const std::string  expected_serialized = []() {
             static_assert(sizeof(wchar_t) >= 2);
             std::string expected(sizeof(wchar_t) * 2, 0);
-            expected[sizeof(wchar_t) - 2] = 0x68;
-            expected[sizeof(wchar_t) - 1] = 0x5C;
-            expected[sizeof(wchar_t) * 2 - 2] = 0x75;
-            expected[sizeof(wchar_t) * 2 - 1] = 0x3A;
+            expected[sizeof(wchar_t) - 2] = to_c(0x68);
+            expected[sizeof(wchar_t) - 1] = to_c(0x5C);
+            expected[sizeof(wchar_t) * 2 - 2] = to_c(0x75);
+            expected[sizeof(wchar_t) * 2 - 1] = to_c(0x3A);
             return expected;
         }();
         const std::string serialized = serialize(key);
+        BOOST_TEST(serialized == expected_serialized);
+    }
+    {
+        const tetengo::trie::default_key_serializer<std::vector<char>> serialize{};
+
+        const std::vector<char> key{ 'T', 'o', 'r', 'i', 'c', 'h', 'o', 's', 'u', 'j', 'i' };
+        const std::string       expected_serialized{ "Torichosuji" };
+        const std::string       serialized = serialize(key);
         BOOST_TEST(serialized == expected_serialized);
     }
 }
