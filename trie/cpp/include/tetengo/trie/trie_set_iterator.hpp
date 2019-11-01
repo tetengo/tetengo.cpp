0b5b294ab0427496ce9a23f3ae484b91640a2bde
kaoru <kaorut@mc.pikara.ne.jp>
2019-11-02 06:56:50 +0900

    Create tetengo::trie::trie_set_iterator #11

---
+++ trie/cpp/include/tetengo/trie/trie_set_iterator.hpp
@@ -0,0 +1,23 @@
+/*! \file
+    \brief A trie set iterator.
+
+    Copyright (C) 2019 kaoru
+*/
+
+#if !defined(TETENGO_TRIE_TRIESETITERATOR_HPP)
+#define TETENGO_TRIE_TRIESETITERATOR_HPP
+
+
+namespace tetengo::trie
+{
+    /*!
+        \brief A trie set iterator.
+    */
+    class trie_set_iterator
+    {};
+
+
+}
+
+
+#endif
---
+++ trie/test/src/test_tetengo.trie.trie_set_iterator.cpp
@@ -0,0 +1,24 @@
+/*!
+    \brief A trie set iterator.
+
+    Copyright (C) 2019 kaoru
+ */
+
+#include <boost/preprocessor.hpp>
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo)
+BOOST_AUTO_TEST_SUITE(trie)
+BOOST_AUTO_TEST_SUITE(trie_set_iterator)
+
+
+BOOST_AUTO_TEST_CASE(construction)
+{
+    BOOST_TEST_PASSPOINT();
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
