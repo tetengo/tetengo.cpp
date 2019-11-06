ce3df75dbc250b515b1e33f53506166ab22fc192
kaoru <kaorut@mc.pikara.ne.jp>
2019-11-07 01:54:28 +0900

    Add a test case check #11

--- trie/test/src/test_tetengo.trie.trie_set.cpp
+++ trie/test/src/test_tetengo.trie.trie_set.cpp
@@ -17,6 +17,13 @@ namespace
 {
     const std::vector<std::string> expected_values_string{ "UTIGOSI", "UTO", "SETA", "SETA" };
 
+    const std::vector<std::wstring> expected_values_string2{
+        std::wstring{ 0x6253, 0x8D8A }, // Utigosi in Kanji
+        std::wstring{ 0x5B87, 0x571F }, // Uto in Kanji
+        std::wstring{ 0x702C, 0x7530 }, // Seta in Kanji
+        std::wstring{ 0x702C, 0x7530 } // Seta in Kanji
+    };
+
 }
 
 
@@ -53,6 +60,15 @@ BOOST_AUTO_TEST_CASE(construction)
         const tetengo::trie::trie_set<std::string> set_{ expected_values_string[0],
                                                          expected_values_string[1],
                                                          expected_values_string[2] };
+
+        BOOST_TEST_REQUIRE(set_.size() == 3);
+        // BOOST_TEST(*set_.find(expected_values_string[0]) == expected_values_string[0]);
+        // BOOST_TEST(*set_.find(expected_values_string[1]) == expected_values_string[1]);
+        // BOOST_TEST(*set_.find(expected_values_string[2]) == expected_values_string[2]);
+    }
+
+    {
+        const tetengo::trie::trie_set<std::wstring> set_{ expected_values_string2.begin(), expected_values_string2.end() };
 
         BOOST_TEST_REQUIRE(set_.size() == 3);
         // BOOST_TEST(*set_.find(expected_values_string[0]) == expected_values_string[0]);
