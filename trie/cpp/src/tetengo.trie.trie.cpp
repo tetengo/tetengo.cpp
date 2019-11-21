600a4cebee43ae6c926dee6131f450098e7ce170
kaoru <kaorut@mc.pikara.ne.jp>
2019-11-21 23:55:27 +0900

    Implement trie #20

--- trie/cpp/src/tetengo.trie.trie.cpp
+++ trie/cpp/src/tetengo.trie.trie.cpp
@@ -4,12 +4,15 @@
     Copyright (C) 2019 kaoru
 */
 
+#include <cstdint>
 #include <memory>
+#include <string_view>
 #include <utility>
 #include <vector>
 
 #include <boost/core/noncopyable.hpp>
 
+#include <tetengo/trie/double_array.hpp>
 #include <tetengo/trie/trie.hpp>
 
 
@@ -20,15 +23,25 @@ namespace tetengo::trie
     public:
         // constructors and destructor
 
-        impl() : m_elements{} {};
+        impl() : m_p_double_array{ std::make_unique<double_array>() } {}
 
-        explicit impl(std::vector<value_type> elements) : m_elements{ std::move(elements) } {}
+        explicit impl(std::vector<value_type> elements) : m_p_double_array{}
+        {
+            std::vector<std::pair<std::string_view, std::int32_t>> double_array_contents{};
+            double_array_contents.reserve(elements.size());
+            for (auto i = static_cast<std::int32_t>(0); i < static_cast<std::int32_t>(elements.size()); ++i)
+            {
+                double_array_contents.emplace_back(elements[i].first, i);
+            }
+            m_p_double_array = std::make_unique<double_array>(double_array_contents);
+
+        }
 
 
     private:
         // variables
 
-        const std::vector<value_type> m_elements;
+        std::unique_ptr<double_array> m_p_double_array;
     };
 
 
