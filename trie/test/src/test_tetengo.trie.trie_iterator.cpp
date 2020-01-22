f4b5e827a71111d7c71dbe3099442a5376985ac2
kaoru <kaorut@mc.pikara.ne.jp>
2020-01-23 02:46:01 +0900

    Implement a test case #28

--- trie/test/src/test_tetengo.trie.trie_iterator.cpp
+++ trie/test/src/test_tetengo.trie.trie_iterator.cpp
@@ -4,8 +4,39 @@
     Copyright (C) 2019 kaoru
  */
 
+#include <iterator>
+#include <string>
+#include <utility>
+
+#include <boost/core/ignore_unused.hpp>
 #include <boost/preprocessor.hpp>
 #include <boost/test/unit_test.hpp>
+
+#include <tetengo/trie/trie.hpp>
+#include <tetengo/trie/trie_iterator.hpp>
+
+
+namespace
+{
+    constexpr char to_c(const unsigned char uc)
+    {
+        return static_cast<char>(uc);
+    }
+
+    static const std::string kumamoto1{
+        to_c(0xE7), to_c(0x86), to_c(0x8A), to_c(0x6), to_c(0x9C), to_c(0xAC) // Kumamoto in Kanji in UTF-8
+    };
+
+    static const std::string tamana1{
+        to_c(0xE7), to_c(0x8E), to_c(0x89), to_c(0xE5), to_c(0x90), to_c(0x8D) // Tamana in Kanji in UTF-8
+    };
+
+    static const std::wstring kumamoto2{ 0x718A, 0x672C }; // Kumamoto in Kanji in UTF-16/32
+
+    static const std::wstring tamana2{ 0x7389, 0x540D }; // Tamana in Kanji in UTF-16/32
+
+
+}
 
 
 BOOST_AUTO_TEST_SUITE(test_tetengo)
@@ -18,13 +49,32 @@ BOOST_AUTO_TEST_CASE(construction)
     BOOST_TEST_PASSPOINT();
 
     {
-        // const tetengo::trie::trie_iterator_impl              iterator_impl{};
-        // const tetengo::trie::trie_iterator<std::string, int> iterator{ std::move(iterator_impl) };
-    } {
+        tetengo::trie::trie_iterator_impl                             impl{};
+        const tetengo::trie::trie_iterator<std::wstring, std::string> iterator{ std::move(impl) };
+    }
+    {
         // TODO: C style API
     }
 
-    BOOST_WARN_MESSAGE(false, "Implement it.");
+    {
+        const tetengo::trie::trie<std::wstring, std::string> trie_{};
+
+        boost::ignore_unused(std::begin(trie_));
+        boost::ignore_unused(std::end(trie_));
+    }
+    {
+        // TODO: C style API
+    }
+
+    {
+        const tetengo::trie::trie<std::wstring, std::string> trie_{ { kumamoto2, kumamoto1 }, { tamana2, tamana1 } };
+
+        boost::ignore_unused(std::begin(trie_));
+        boost::ignore_unused(std::end(trie_));
+    }
+    {
+        // TODO: C style API
+    }
 }
 
 BOOST_AUTO_TEST_CASE(operator_dereference)
