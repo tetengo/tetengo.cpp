e81c80034d3a0fa23af0aba1417fbd6b959d4b37
kaoru <kaorut@mc.pikara.ne.jp>
2020-02-23 03:31:06 +0900

    Refactor #33

--- executable/make_dict/src/main.cpp
+++ executable/make_dict/src/main.cpp
@@ -9,14 +9,16 @@
 #include <cassert>
 #include <cstdint>
 #include <exception>
+#include <filesystem>
 #include <fstream> // IWYU pragma: keep
-#include <functional>
 #include <iostream>
 #include <iterator>
 #include <limits>
+#include <memory>
 #include <stdexcept>
 #include <string>
 #include <string_view>
+#include <type_traits>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -86,6 +88,50 @@ namespace
         i_value->second.emplace_back(offset, length);
     }
 
+    std::unordered_map<std::string, std::vector<std::pair<std::size_t, std::size_t>>>
+    load_lex_csv(const std::filesystem::path& lex_csv_path)
+    {
+        std::ifstream stream{ lex_csv_path };
+        if (!stream)
+        {
+            throw std::ios_base::failure{ "Can't open the input file." };
+        }
+
+        std::unordered_map<std::string, std::vector<std::pair<std::size_t, std::size_t>>> word_offset_map{};
+
+        std::cerr << "Loading UniDic lex.csv..." << std::endl;
+        auto line_head = static_cast<std::size_t>(0);
+        for (auto i = static_cast<std::size_t>(0); stream; ++i)
+        {
+            std::string line{};
+            std::getline(stream, line);
+            if (line.empty())
+            {
+                line_head += line.length() + 1;
+                continue;
+            }
+            const auto elements = split(line, ',');
+            if (elements.size() != 33)
+            {
+                std::cerr << boost::format{ "%8d: %s" } % i % elements[0] << "    \n" << std::flush;
+                throw std::runtime_error{ "Invalid UniDic lex.csv format." };
+            }
+
+            insert_word_offset_to_map(elements[12], line_head, line.length() + 1, word_offset_map);
+            insert_word_offset_to_map(elements[24], line_head, line.length() + 1, word_offset_map);
+
+            if (i % 10000 == 0)
+            {
+                std::cerr << boost::format{ "%8d: %s" } % i % elements[0] << "    \r" << std::flush;
+            }
+
+            line_head += line.length() + 1;
+        }
+        std::cerr << "Done.        " << std::endl;
+
+        return word_offset_map;
+    }
+
     class trie_building_observer
     {
     public:
@@ -103,6 +149,21 @@ namespace
     private:
         std::size_t m_index;
     };
+
+    std::unique_ptr<tetengo::trie::trie<std::string_view, std::vector<std::pair<std::size_t, std::size_t>>>>
+    build_trie(const std::unordered_map<std::string, std::vector<std::pair<std::size_t, std::size_t>>>& word_offset_map)
+    {
+        std::cerr << "Building trie..." << std::endl;
+        auto p_trie =
+            std::make_unique<tetengo::trie::trie<std::string_view, std::vector<std::pair<std::size_t, std::size_t>>>>(
+                std::make_move_iterator(std::begin(word_offset_map)),
+                std::make_move_iterator(std::end(word_offset_map)),
+                tetengo::trie::default_serializer<std::string_view>{},
+                tetengo::trie::trie<std::string_view, std::vector<std::pair<std::size_t, std::size_t>>>::
+                    building_observer_set_type{ trie_building_observer{}, []() {} });
+        std::cerr << "Done.        " << std::endl;
+        return p_trie;
+    }
 
     std::string serialize_size_t(const std::size_t s)
     {
@@ -150,6 +211,20 @@ namespace
         return serialize_vector_of_pair_of_size_t(*p_value);
     }
 
+    void serialize_trie(
+        const tetengo::trie::trie<std::string_view, std::vector<std::pair<std::size_t, std::size_t>>>& trie_,
+        const std::filesystem::path&                                                                   trie_bin_path)
+    {
+        std::cerr << "Serializing trie..." << std::endl;
+        std::ofstream output_stream{ trie_bin_path, std::ios_base::binary };
+        if (!output_stream)
+        {
+            throw std::ios_base::failure{ "Can't open the output file." };
+        }
+        trie_.get_storage().serialize(output_stream, serialize_value);
+        std::cerr << "Done.        " << std::endl;
+    }
+
 
 }
 
@@ -164,62 +239,9 @@ int main(const int argc, char** const argv)
             return 0;
         }
 
-        std::ifstream stream{ argv[1] };
-        if (!stream)
-        {
-            throw std::ios_base::failure{ "Can't open the input file." };
-        }
-
-        std::unordered_map<std::string, std::vector<std::pair<std::size_t, std::size_t>>> word_offset_map{};
-
-        std::cerr << "Loading UniDic lex.csv..." << std::endl;
-        auto line_head = static_cast<std::size_t>(0);
-        for (auto i = static_cast<std::size_t>(0); stream; ++i)
-        {
-            std::string line{};
-            std::getline(stream, line);
-            if (line.empty())
-            {
-                line_head += line.length() + 1;
-                continue;
-            }
-            const auto elements = split(line, ',');
-            if (elements.size() != 33)
-            {
-                std::cerr << boost::format{ "%8d: %s" } % i % elements[0] << "    \n" << std::flush;
-                throw std::runtime_error{ "Invalid UniDic lex.csv format." };
-            }
-
-            insert_word_offset_to_map(elements[12], line_head, line.length() + 1, word_offset_map);
-            insert_word_offset_to_map(elements[24], line_head, line.length() + 1, word_offset_map);
-
-            if (i % 10000 == 0)
-            {
-                std::cerr << boost::format{ "%8d: %s" } % i % elements[0] << "    \r" << std::flush;
-            }
-
-            line_head += line.length() + 1;
-        }
-        std::cerr << "Done.        " << std::endl;
-
-        std::cerr << "Building trie..." << std::endl;
-        const tetengo::trie::trie<std::string_view, std::vector<std::pair<std::size_t, std::size_t>>> trie_{
-            std::make_move_iterator(std::begin(word_offset_map)),
-            std::make_move_iterator(std::end(word_offset_map)),
-            tetengo::trie::default_serializer<std::string_view>{},
-            tetengo::trie::trie<std::string_view, std::vector<std::pair<std::size_t, std::size_t>>>::
-                building_observer_set_type{ trie_building_observer{}, []() {} }
-        };
-        std::cerr << "Done.        " << std::endl;
-
-        std::cerr << "Serializing trie..." << std::endl;
-        std::ofstream output_stream{ argv[2], std::ios_base::binary };
-        if (!output_stream)
-        {
-            throw std::ios_base::failure{ "Can't open the output file." };
-        }
-        trie_.get_storage().serialize(output_stream, serialize_value);
-        std::cerr << "Done.        " << std::endl;
+        const auto word_offset_map = load_lex_csv(argv[1]);
+        const auto p_trie = build_trie(word_offset_map);
+        serialize_trie(*p_trie, argv[2]);
 
         return 0;
     }
