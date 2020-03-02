ac772eb24f963a043794ad17666b5c13e7431e4f
kaoru <kaorut@mc.pikara.ne.jp>
2020-03-03 00:48:34 +0900

    Implement the C interface of tetengo::trie::trie_iterator #10

---
+++ library/trie/c/include/tetengo/trie/trieIterator.h
@@ -0,0 +1,17 @@
+/*! \file
+    \brief A trie iterator.
+
+    Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
+*/
+
+#if !defined(TETENGO_TRIE_TRIEITERATOR_H)
+#define TETENGO_TRIE_TRIEITERATOR_H
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+
+#if defined(__cplusplus)
+}
+#endif
+#endif
---
+++ library/trie/c/src/tetengo_trie_trieIterator.cpp
@@ -0,0 +1,7 @@
+/*! \file
+    \brief A trie iterator.
+
+    Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
+*/
+
+#include <tetengo/trie/trieIterator.h>
