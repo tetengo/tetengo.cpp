4273a7e60518755b6c91e3bdad754d602f0b95d8
kaoru <kaorut@mc.pikara.ne.jp>
2020-03-31 02:29:24 +0900

    Create source files #45

---
+++ library/lattice/cpp/include/tetengo/lattice/vocabulary.hpp
@@ -0,0 +1,11 @@
+/*! \file
+    \brief A double array.
+
+    Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
+*/
+
+#if !defined(TETENGO_LATTICE_VOCABULARY_HPP)
+#define TETENGO_LATTICE_VOCABULARY_HPP
+
+
+#endif
---
+++ library/lattice/test/src/master.cpp
@@ -0,0 +1,25 @@
+/*! \file
+    \brief The master test suite.
+
+    Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
+ */
+
+#define BOOST_TEST_MODULE "tetengo-lattice.test"
+#include <boost/test/unit_test.hpp> // IWYU pragma: keep
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo)
+BOOST_AUTO_TEST_SUITE(lattice)
+BOOST_AUTO_TEST_SUITE(vocabulary)
+
+
+BOOST_AUTO_TEST_CASE(null_building_observer_set)
+{
+    BOOST_TEST_PASSPOINT();
+
+    BOOST_WARN_MESSAGE(false, "Implement it.");
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
