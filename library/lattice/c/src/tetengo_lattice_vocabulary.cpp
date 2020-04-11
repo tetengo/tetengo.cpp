2e150d3dce6da71860772463004256c4539bd83f
kaoru <kaorut@mc.pikara.ne.jp>
2020-04-11 16:28:44 +0900

    Implement the C interface of tetengo::lattice::vocabulary #46

--- library/lattice/c/src/tetengo_lattice_vocabulary.cpp
+++ library/lattice/c/src/tetengo_lattice_vocabulary.cpp
@@ -4,8 +4,22 @@
     Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
 */
 
-#include <tetengo/lattice/vocabulary.h>
+#include <memory>
+#include <utility>
 
+#include <tetengo/lattice/unordered_map_vocabulary.hpp>
+#include <tetengo/lattice/vocabulary.h>
+#include <tetengo/lattice/vocabulary.hpp>
+
+
+struct tetengo_lattice_vocabulary_tag
+{
+    std::unique_ptr<tetengo::lattice::vocabulary> p_cpp_vocabulary;
+
+    explicit tetengo_lattice_vocabulary_tag(std::unique_ptr<tetengo::lattice::vocabulary>&& p_cpp_vocabulary) :
+    p_cpp_vocabulary{ std::move(p_cpp_vocabulary) }
+    {}
+};
 
 tetengo_lattice_vocabulary* tetengo_lattice_vocabulary_createUnorderedMapVocabulary(
     const char* const* const /*p_keys*/,
