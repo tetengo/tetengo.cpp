2f68972dd3a0dcd7467a482d532d2aaa2fa874e2
kaoru <kaorut@mc.pikara.ne.jp>
2020-04-29 16:46:59 +0900

    Create tetengo::lattice::connection #51

---
+++ library/lattice/c/include/tetengo/lattice/connection.h
@@ -0,0 +1,32 @@
+/*! \file
+    \brief A connection.
+
+    Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
+ */
+
+#if !defined(TETENGO_LATTICE_CONNECTION_H)
+#define TETENGO_LATTICE_CONNECTION_H
+
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+/*!
+    \brief A connection.
+*/
+typedef struct tetengo_lattice_connection_tag
+{
+    //! A cost.
+    int cost;
+
+
+} tetengo_lattice_connection;
+
+
+#if defined(__cplusplus)
+}
+#endif
+
+
+#endif
---
+++ library/lattice/cpp/include/tetengo/lattice/connection.hpp
@@ -0,0 +1,49 @@
+/*! \file
+    \brief A connection.
+
+    Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
+*/
+
+#if !defined(TETENGO_LATTICE_CONNECTION_HPP)
+#define TETENGO_LATTICE_CONNECTION_HPP
+
+
+namespace tetengo::lattice
+{
+    /*!
+        \brief A connection.
+    */
+    class connection
+    {
+    public:
+        // constructors
+
+        /*!
+            \brief Creates a connection.
+
+            \param cost A cost.
+        */
+        explicit connection(int cost);
+
+
+        // functions
+
+        /*!
+            \brief Returns the cost.
+
+            \return The cost.
+        */
+        int cost() const;
+
+
+    private:
+        // variables
+
+        int m_cost;
+    };
+
+
+}
+
+
+#endif
