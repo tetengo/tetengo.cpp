577899f1bcf3c581a3162b639593b71cc45f057a
kaoru <kaorut@mc.pikara.ne.jp>
2020-05-30 15:45:33 +0900

    Implement tetengo::lattice::constraint_element #64

--- library/lattice/cpp/src/tetengo.lattice.constraint_element.cpp
+++ library/lattice/cpp/src/tetengo.lattice.constraint_element.cpp
@@ -4,7 +4,20 @@
     Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
 */
 
+#include <tetengo/lattice/constraint_element.hpp>
+
 
 namespace tetengo::lattice
 {
+    class node;
+
+
+    constraint_element::~constraint_element() = default;
+
+    int constraint_element::matches(const node& node_) const
+    {
+        return matches_impl(node_);
+    }
+
+
 }
--- library/lattice/test/src/test_tetengo.lattice.constraint_element.cpp
+++ library/lattice/test/src/test_tetengo.lattice.constraint_element.cpp
@@ -4,8 +4,39 @@
     Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
  */
 
+#include <any>
+#include <string_view>
+#include <vector>
+
 #include <boost/preprocessor.hpp>
 #include <boost/test/unit_test.hpp>
+
+#include <tetengo/lattice/constraint_element.hpp>
+#include <tetengo/lattice/node.hpp>
+
+
+namespace
+{
+    class concrete_constraint_element : public tetengo::lattice::constraint_element
+    {
+    private:
+        // virtual functions
+
+        virtual int matches_impl(const tetengo::lattice::node& node_) const override
+        {
+            if (node_.preceding_step() > 4)
+            {
+                return -1;
+            }
+            else
+            {
+                return 4 - static_cast<int>(node_.preceding_step());
+            }
+        }
+    };
+
+
+}
 
 
 BOOST_AUTO_TEST_SUITE(test_tetengo)
@@ -17,7 +48,40 @@ BOOST_AUTO_TEST_CASE(construction)
 {
     BOOST_TEST_PASSPOINT();
 
-    BOOST_WARN_MESSAGE(false, "Implement it.");
+    {
+        const concrete_constraint_element element{};
+    }
+}
+
+BOOST_AUTO_TEST_CASE(matches)
+{
+    BOOST_TEST_PASSPOINT();
+
+    {
+        const concrete_constraint_element element{};
+
+        {
+            const std::any               value{ 42 };
+            const std::vector<int>       preceding_edge_costs{ 3, 1, 4, 1, 5, 9, 2, 6 };
+            const tetengo::lattice::node node_{ "mizuho", &value, 1, &preceding_edge_costs, 5, 24, 2424 };
+
+            BOOST_TEST(element.matches(node_) > 0);
+        }
+        {
+            const std::any               value{ 42 };
+            const std::vector<int>       preceding_edge_costs{ 3, 1, 4, 1, 5, 9, 2, 6 };
+            const tetengo::lattice::node node_{ "sakura", &value, 4, &preceding_edge_costs, 5, 24, 2424 };
+
+            BOOST_TEST(element.matches(node_) == 0);
+        }
+        {
+            const std::any               value{ 42 };
+            const std::vector<int>       preceding_edge_costs{ 3, 1, 4, 1, 5, 9, 2, 6 };
+            const tetengo::lattice::node node_{ "tsubame", &value, 5, &preceding_edge_costs, 5, 24, 2424 };
+
+            BOOST_TEST(element.matches(node_) < 0);
+        }
+    }
 }
 
 
