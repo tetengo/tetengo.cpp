3de5ed88a4df3e4fccdec78a87da6902464e181d
kaoru <kaorut@mc.pikara.ne.jp>
2020-06-20 11:19:04 +0900

    Implement transfer_trains #58

--- executable/transfer_trains/src/main.cpp
+++ executable/transfer_trains/src/main.cpp
@@ -1,1 +1,53 @@
-int main() {}
+/*! \file
+    \brief A train transfer guide.
+
+    Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
+ */
+
+#include <exception>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+
+
+namespace
+{
+    std::unique_ptr<std::istream> create_input_stream(const std::filesystem::path& path)
+    {
+        auto p_stream = std::make_unique<std::ifstream>(path);
+        if (!*p_stream)
+        {
+            throw std::ios_base::failure{ "Can't open " + path.string() };
+        }
+        return p_stream;
+    }
+}
+
+
+int main(const int argc, char** const argv)
+{
+    try
+    {
+        if (argc <= 1)
+        {
+            std::cout << "Usage: transfer_trains timetable.txt" << std::endl;
+            return 0;
+        }
+
+        const auto p_input_stream = create_input_stream(argv[1]);
+
+        return 0;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << "Error: unknown error." << std::endl;
+        return 1;
+    }
+}
