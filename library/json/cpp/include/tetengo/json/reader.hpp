f9c9d42bbfaef7246e7fab0b7d0c361ef99776bb
kaoru <kaorut@mc.pikara.ne.jp>
2020-07-27 00:16:02 +0900

    Implement tetengo::json::reader #79

--- library/json/cpp/include/tetengo/json/reader.hpp
+++ library/json/cpp/include/tetengo/json/reader.hpp
@@ -7,14 +7,63 @@
 #if !defined(TETENGO_JSON_READER_HPP)
 #define TETENGO_JSON_READER_HPP
 
+#include <boost/core/noncopyable.hpp>
+
 
 namespace tetengo::json
 {
     /*!
         \brief A reader.
     */
-    class reader
-    {};
+    class reader : private boost::noncopyable
+    {
+    public:
+        // constructors and destructor
+
+        /*!
+            \brief Destroys the reader.
+        */
+        virtual ~reader();
+
+
+        // functions
+
+        /*!
+            \brief Returns true when the next character exists.
+
+            \return true  When the next character exists.
+            \return false Otherwise.
+        */
+        bool has_next() const;
+
+        /*!
+            \brief Returns the current character.
+
+            \return The current character.
+
+            \throw std::logic_error When current position is beyond the
+                                    termination point.
+        */
+        char get() const;
+
+        /*!
+            \brief Moves to the next character.
+
+            \throw std::logic_error When current position is beyond the
+                                    termination point.
+        */
+        void next();
+
+
+    private:
+        // virtual functions
+
+        virtual bool has_next_impl() const = 0;
+
+        virtual char get_impl() const = 0;
+
+        virtual void next_impl() = 0;
+    };
 
 
 }
--- library/json/cpp/src/tetengo.json.reader.cpp
+++ library/json/cpp/src/tetengo.json.reader.cpp
@@ -4,7 +4,27 @@
     Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
 */
 
+#include <tetengo/json/reader.hpp>
+
 
 namespace tetengo::json
 {
+    reader::~reader() = default;
+
+    bool reader::has_next() const
+    {
+        return has_next_impl();
+    }
+
+    char reader::get() const
+    {
+        return get_impl();
+    }
+
+    void reader::next()
+    {
+        next_impl();
+    }
+
+
 }
