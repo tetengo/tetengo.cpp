03f32da8b97e7a32fb52e95f1ba590a5024b6ade
kaoru <kaorut@mc.pikara.ne.jp>
2020-08-12 02:09:10 +0900

    Create tetengo::json::json_grammar #84

---
+++ library/json/cpp/include/tetengo/json/json_grammar.hpp
@@ -0,0 +1,23 @@
+/*! \file
+    \brief A JSON grammar.
+
+    Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
+*/
+
+#if !defined(TETENGO_JSON_JSONGRAMMAR_HPP)
+#define TETENGO_JSON_JSONGRAMMAR_HPP
+
+
+namespace tetengo::json
+{
+    /*!
+        \brief A JSON grammar.
+    */
+    class json_grammar
+    {};
+
+
+}
+
+
+#endif
---
+++ library/json/cpp/src/tetengo.json.json_grammar.cpp
@@ -0,0 +1,10 @@
+/*! \file
+    \brief A JSON grammar.
+
+    Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
+*/
+
+
+namespace tetengo::json
+{
+}
