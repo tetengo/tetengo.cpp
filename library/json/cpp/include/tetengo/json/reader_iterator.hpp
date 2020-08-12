156a2384546afee40aeb81bb2b318aedd9cc5394
kaoru <kaorut@mc.pikara.ne.jp>
2020-08-12 12:37:08 +0900

    Create tetengo::json::reader_iterator #85

---
+++ library/json/cpp/include/tetengo/json/reader_iterator.hpp
@@ -0,0 +1,23 @@
+/*! \file
+    \brief A reader iterato.
+
+    Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
+*/
+
+#if !defined(TETENGO_JSON_READERITERATOR_HPP)
+#define TETENGO_JSON_READERITERATOR_HPP
+
+
+namespace tetengo::json
+{
+    /*!
+        \brief A reader iterator.
+    */
+    class reader_iterator
+    {};
+
+
+}
+
+
+#endif
---
+++ library/json/cpp/src/tetengo.json.reader_iterator.cpp
@@ -0,0 +1,10 @@
+/*! \file
+    \brief A reader iterator.
+
+    Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
+*/
+
+
+namespace tetengo::json
+{
+}
