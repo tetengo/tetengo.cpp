c97abd428dd6160913c0584c8d296c8a49d41e21
kaoru <kaorut@mc.pikara.ne.jp>
2020-08-21 22:18:59 +0900

    Implement the C interface of tetengo::json::element #81

--- library/json/c/include/tetengo/json/element.h
+++ library/json/c/include/tetengo/json/element.h
@@ -12,6 +12,76 @@
 extern "C" {
 #endif
 
+/*!
+    \brief Returns the type name of string.
+
+    \return The type name.
+*/
+int tetengo_json_element_typeName_string();
+
+/*!
+    \brief Returns the type name of number.
+
+    \return The type name.
+*/
+int tetengo_json_element_typeName_number();
+
+/*!
+    \brief Returns the type name of boolean.
+
+    \return The type name.
+*/
+int tetengo_json_element_typeName_boolean();
+
+/*!
+    \brief Returns the type name of null.
+
+    \return The type name.
+*/
+int tetengo_json_element_typeName_null();
+
+/*!
+    \brief Returns the type name of object.
+
+    \return The type name.
+*/
+int tetengo_json_element_typeName_object();
+
+/*!
+    \brief Returns the type name of member.
+
+    \return The type name.
+*/
+int tetengo_json_element_typeName_member();
+
+/*!
+    \brief Returns the type name of array.
+
+    \return The type name.
+*/
+int tetengo_json_element_typeName_array();
+
+/*!
+    \brief Returns the type category of primitive.
+
+    \return The type category.
+*/
+int tetengo_json_element_typeCategory_primitive();
+
+/*!
+    \brief Returns the type category of opening structure.
+
+    \return The type category.
+*/
+int tetengo_json_element_typeCategory_structureOpen();
+
+/*!
+    \brief Returns the type category of closing structure.
+
+    \return The type category.
+*/
+int tetengo_json_element_typeCategory_structureClose();
+
 
 #if defined(__cplusplus)
 }
--- library/json/c/src/tetengo_json_element.cpp
+++ library/json/c/src/tetengo_json_element.cpp
@@ -3,3 +3,57 @@
 
     Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
 */
+
+#include <tetengo/json/element.h>
+#include <tetengo/json/element.hpp>
+
+
+int tetengo_json_element_typeName_string()
+{
+    return static_cast<int>(tetengo::json::element::type_name_type::string);
+}
+
+int tetengo_json_element_typeName_number()
+{
+    return static_cast<int>(tetengo::json::element::type_name_type::number);
+}
+
+int tetengo_json_element_typeName_boolean()
+{
+    return static_cast<int>(tetengo::json::element::type_name_type::boolean);
+}
+
+int tetengo_json_element_typeName_null()
+{
+    return static_cast<int>(tetengo::json::element::type_name_type::null);
+}
+
+int tetengo_json_element_typeName_object()
+{
+    return static_cast<int>(tetengo::json::element::type_name_type::object);
+}
+
+int tetengo_json_element_typeName_member()
+{
+    return static_cast<int>(tetengo::json::element::type_name_type::member);
+}
+
+int tetengo_json_element_typeName_array()
+{
+    return static_cast<int>(tetengo::json::element::type_name_type::array);
+}
+
+int tetengo_json_element_typeCategory_primitive()
+{
+    return static_cast<int>(tetengo::json::element::type_category_type::primitive);
+}
+
+int tetengo_json_element_typeCategory_structureOpen()
+{
+    return static_cast<int>(tetengo::json::element::type_category_type::structure_open);
+}
+
+int tetengo_json_element_typeCategory_structureClose()
+{
+    return static_cast<int>(tetengo::json::element::type_category_type::structure_close);
+}
