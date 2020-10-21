538a27d08ebdbaed998d6683e6facc0af129953d
kaoru <kaorut@mc.pikara.ne.jp>
2020-10-22 00:59:47 +0900

    Move load_timetable() to the separated source #92

--- executable/json2timetable/src/main.c
+++ executable/json2timetable/src/main.c
@@ -4,34 +4,10 @@
     Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
 */
 
+#include "loadTimetable.h"
 #include "printTimetable.h"
 #include "timetable.h"
 
-
-static timetable_t* load_timetable()
-{
-    static const char* p_stations[] = { "HogeHoge", "F", "Piyo" };
-    timetable_t*       p_timetable = timetable_create(p_stations, sizeof(p_stations) / sizeof(const char*));
-
-    {
-        static const int times[] = { -1, 600, 730 };
-        timetable_addTrain(p_timetable, "101M", times);
-    }
-    {
-        static const int times[] = { 1000, 1200, 1330 };
-        timetable_addTrain(p_timetable, "103M", times);
-    }
-    {
-        static const int times[] = { 1600, -1, 1900 };
-        timetable_addTrain(p_timetable, "5M", times);
-    }
-    {
-        static const int times[] = { 2200, 0, -1 };
-        timetable_addTrain(p_timetable, "3107M", times);
-    }
-
-    return p_timetable;
-}
 
 int main()
 {
