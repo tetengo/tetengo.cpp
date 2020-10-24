464628b714becc9ca6e549d3bf4b303830461596
kaoru <kaorut@mc.pikara.ne.jp>
2020-10-24 10:22:58 +0900

    Implement load_timetable() #92

--- executable/json2timetable/src/loadTimetable.c
+++ executable/json2timetable/src/loadTimetable.c
@@ -6,6 +6,10 @@
 
 #include "loadTimetable.h"
 
+#include <stdio.h>
+
+#include <tetengo/json/element.h>
+#include <tetengo/json/jsonParser.h>
 #include <tetengo/json/reader.h>
 
 #include "timetable.h"
@@ -14,8 +18,27 @@
 timetable_t* load_timetable(const char* const timetable_file_path)
 {
     {
-        const tetengo_json_reader_t* const p_reader = tetengo_json_reader_createStreamReader(timetable_file_path, 1000);
-        tetengo_json_reader_destroy(p_reader);
+        tetengo_json_reader_t* const p_reader =
+            tetengo_json_reader_createLineCountingReader(tetengo_json_reader_createStreamReader(
+                timetable_file_path, tetengo_json_reader_streamReaderDefaultBufferCapacity()));
+
+        tetengo_json_jsonParser_t* const p_parser =
+            tetengo_json_jsonParser_create(p_reader, tetengo_json_jsonParser_defaultBufferCapacity());
+
+        while (tetengo_json_jsonParser_hasNext(p_parser))
+        {
+            const tetengo_json_element_t* const p_element = tetengo_json_jsonParser_peek(p_parser);
+            /* tetengo_json_location_t             location = { 0 }; */
+            /* tetengo_json_reader_getLocation(p_reader, &location); */
+
+            const tetengo_json_element_type_t* const p_element_type = tetengo_json_element_type(p_element);
+            printf("%d: %s\n", p_element_type->name, tetengo_json_element_value(p_element));
+            /* printf("  %.*s (%d)\n", (int)location.line_length, location.line, (int)location.column_index); */
+
+            tetengo_json_jsonParser_next(p_parser);
+        }
+
+        tetengo_json_jsonParser_destroy(p_parser);
     }
 
     {
