114203e58ba6d70ae0be57864f67e2fcc303a20a
kaoru <kaorut@mc.pikara.ne.jp>
2020-11-23 22:13:57 +0900

    Create tetengo::platform_dependent #110

---
+++ library/platform_dependent/cpp/include/tetengo/platform_dependent/text_encoder.hpp
@@ -0,0 +1,16 @@
+/*! \file
+    \brief A text encoder.
+
+    Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
+*/
+
+#if !defined(TETENGO_PLATFORMDEPENDENT_TEXTENCODER_HPP)
+#define TETENGO_PLATFORMDEPENDENT_TEXTENCODER_HPP
+
+
+namespace tetengo::platform_dependent
+{
+}
+
+
+#endif
