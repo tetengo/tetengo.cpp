23c7af5042dc1893951c7195976d4e7ac3b380d6
kaoru <kaorut@mc.pikara.ne.jp>
2020-11-28 16:02:34 +0900

    Implement test case strings #110

--- library/platform_dependent/test/src/test_tetengo.platform_dependent.text_encoder.cpp
+++ library/platform_dependent/test/src/test_tetengo.platform_dependent.text_encoder.cpp
@@ -4,10 +4,87 @@
     Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
 */
 
+#include <string>
+
 #include <boost/preprocessor.hpp>
 #include <boost/test/unit_test.hpp>
 
 #include <tetengo/platform_dependent/text_encoder.hpp>
+
+
+namespace
+{
+    constexpr char operator""_c(const unsigned long long int uc)
+    {
+        return static_cast<char>(uc);
+    }
+
+    // tokkyu rapi:t beta 25-go kansai-kuko yuki (rapi:t limited express No. 25 bound for Kansai Airport)
+    const std::string string1_utf8{
+        // clang-format off
+        0xE7_c, 0x89_c, 0xB9_c, 0xE6_c, 0x80_c, 0xA5_c,
+        0xE3_c, 0x83_c, 0xA9_c, 0xE3_c, 0x83_c, 0x94_c, 0xE3_c, 0x83_c, 0xBC_c, 0xE3_c, 0x83_c, 0x88_c,
+        0xCE_c, 0xB2_c,
+        0x32_c, 0x35_c, 0xE5_c, 0x8F_c, 0xB7_c,
+        0xE9_c, 0x96_c, 0xA2_c, 0xE8_c, 0xA5_c, 0xBF_c, 0xE7_c, 0xA9_c, 0xBA_c, 0xE6_c, 0xB8_c, 0xAF_c,
+        0xE3_c, 0x82_c, 0x86_c, 0xE3_c, 0x81_c, 0x8D_c,
+        // clang-format on
+    };
+
+    // tokkyu rapi:t beta 25-go kansai-kuko yuki (rapi:t limited express No. 25 bound for Kansai Airport)
+    const std::string string1_cp932{
+        // clang-format off
+        0x93_c, 0xC1_c, 0x8B_c, 0x7D_c,
+        0x83_c, 0x89_c, 0x83_c, 0x73_c, 0x81_c, 0x5B_c, 0x83_c, 0x67_c,
+        0x83_c, 0xC0_c,
+        0x32_c, 0x35_c, 0x8D_c, 0x86_c,
+        0x8A_c, 0xD6_c, 0x90_c, 0xBC_c, 0x8B_c, 0xF3_c, 0x8D_c, 0x60_c,
+        0x82_c, 0xE4_c, 0x82_c, 0xAB_c,
+        // clang-format on
+    };
+
+    // wave-dash and back-slash
+    const std::string string2_utf8{
+        // clang-format off
+        0xEF_c, 0xBD_c, 0x9E_c,
+        0x5C_c,
+        // clang-format on
+    };
+
+    // wave-dash and back-slash
+    const std::string string2_cp932{
+        // clang-format off
+        0x81_c, 0x60_c,
+        0x5C_c,
+        // clang-format on
+    };
+
+    // shinkansen (emoji) kara romendensha (emoji) e (From Shinkansen to tram)
+    const std::string string3_utf8{
+        // clang-format off
+        0xE6_c, 0x96_c, 0xB0_c, 0xE5_c, 0xB9_c, 0xB9_c, 0xE7_c, 0xB7_c, 0x9A_c,
+        0xF0_c, 0x9F_c, 0x9A_c, 0x85_c,
+        0xE3_c, 0x81_c, 0x8B_c, 0xE3_c, 0x82_c, 0x89_c,
+        0xE8_c, 0xB7_c, 0xAF_c, 0xE9_c, 0x9D_c, 0xA2_c, 0xE9_c, 0x9B_c, 0xBB_c, 0xE8_c, 0xBB_c, 0x8A_c,
+        0xF0_c, 0x9F_c, 0x9A_c, 0x8B_c,
+        0xE3_c, 0x81_c, 0xB8_c,
+        // clang-format on
+    };
+
+    // shinkansen (emoji) kara romendensha (emoji) e (From Shinkansen to tram)
+    const std::string string3_cp932{
+        // clang-format off
+        0x90_c, 0x56_c, 0x8A_c, 0xB2_c, 0x90_c, 0xFC_c,
+        0x3F_c,
+        0x82_c, 0xA9_c, 0x82_c, 0xE7_c,
+        0x98_c, 0x48_c, 0x96_c, 0xCA_c, 0x93_c, 0x64_c, 0x8E_c, 0xD4_c,
+        0x3F_c,
+        0x82_c, 0xD6_c,
+        // clang-format on
+    };
+
+
+}
 
 
 BOOST_AUTO_TEST_SUITE(test_tetengo)
@@ -16,6 +93,20 @@ BOOST_AUTO_TEST_SUITE(text_encoder)
 
 
 BOOST_AUTO_TEST_CASE(instance)
+{
+    BOOST_TEST_PASSPOINT();
+
+    tetengo::platform_dependent::text_encoder::instance();
+}
+
+BOOST_AUTO_TEST_CASE(encode_to_cp932)
+{
+    BOOST_TEST_PASSPOINT();
+
+    tetengo::platform_dependent::text_encoder::instance();
+}
+
+BOOST_AUTO_TEST_CASE(decode_from_cp932)
 {
     BOOST_TEST_PASSPOINT();
 
