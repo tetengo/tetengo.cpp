c13a82ec32e8919f8aeecd5f770a2f6588a78b25
kaoru <kaorut@mc.pikara.ne.jp>
2020-12-01 23:29:33 +0900

    Create tetengo::text::encoding::utf8 #108

---
+++ library/text/cpp/include/tetengo/text/encoding/utf8.hpp
@@ -0,0 +1,89 @@
+/*! \file
+    \brief A UTF-8 encoding.
+
+    Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
+*/
+
+#if !defined(TETENGO_TEXT_ENCODING_UTF8_HPP)
+#define TETENGO_TEXT_ENCODING_UTF8_HPP
+
+#include <memory>
+#include <string_view>
+
+#include <boost/core/noncopyable.hpp>
+
+
+namespace tetengo::text::encoding
+{
+    /*!
+        \brief A UTF-8 encoding.
+    */
+    class utf8 : private boost::noncopyable
+    {
+    public:
+        // types
+
+        //! The encoded string type.
+        using encoded_string_type = std::string_view;
+
+
+        // static functions
+
+        /*!
+            \brief Returns the instance.
+
+            \return The instance.
+        */
+        [[nodiscard]] static const utf8& instance();
+
+
+        // constructors and destructor
+
+        /*!
+            \brief Destroys the encoding.
+        */
+        ~utf8();
+
+
+        // functions
+
+        /*!
+            \brief Encodes a UTF-8 string to this encoding.
+
+            \param utf8 A UTF-8 string.
+
+            \return An encoded string.
+        */
+        [[nodiscard]] encoded_string_type encode(const std::string_view& utf8) const;
+
+        /*!
+            \brief Decodes a string in this encoding to UTF-8.
+
+            \param string_ A string in this encoding.
+
+            \return A decoded string.
+        */
+        [[nodiscard]] std::string_view decode(const encoded_string_type& string_) const;
+
+
+    private:
+        // types
+
+        class impl;
+
+
+        // variables
+
+        const std::unique_ptr<impl> m_p_impl;
+
+
+        // constructors
+
+        utf8();
+    };
+
+
+}
+
+
+#endif
---
+++ library/text/cpp/src/tetengo.text.encoding.utf8.cpp
@@ -0,0 +1,68 @@
+/*! \file
+    \brief A UTF-8 encoding.
+
+    Copyright (C) 2019-2020 kaoru  https://www.tetengo.org/
+*/
+
+#include <memory>
+#include <string_view>
+
+#include <boost/core/noncopyable.hpp>
+
+#include <tetengo/text/encoding/utf8.hpp>
+
+
+namespace tetengo::text::encoding
+{
+    class utf8::impl : private boost::noncopyable
+    {
+    public:
+        // types
+
+        using encoded_string_type = typename utf8::encoded_string_type;
+
+
+        // static functions
+
+        static const utf8& instance()
+        {
+            static const utf8 singleton{};
+            return singleton;
+        }
+
+
+        // functions
+
+        encoded_string_type encode(const std::string_view& utf8) const
+        {
+            return utf8;
+        }
+
+        std::string_view decode(const encoded_string_type& string_) const
+        {
+            return string_;
+        }
+    };
+
+
+    const utf8& utf8::instance()
+    {
+        return impl::instance();
+    }
+
+    utf8::~utf8() = default;
+
+    utf8::encoded_string_type utf8::encode(const std::string_view& utf8) const
+    {
+        return m_p_impl->encode(utf8);
+    }
+
+    std::string_view utf8::decode(const encoded_string_type& string_) const
+    {
+        return m_p_impl->decode(string_);
+    }
+
+    utf8::utf8() : m_p_impl{ std::make_unique<impl>() } {}
+
+
+}
