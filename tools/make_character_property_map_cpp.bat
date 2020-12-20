fef8f1aa28e2b3dd95f0c9ca6caa41d2e52ec95a
kaoru <kaorut@mc.pikara.ne.jp>
2020-12-20 09:41:51 +0900

    Modify make_character_property_map.(bat|py|sh) #111

--- tools/make_character_property_map_cpp.bat
+++ tools/make_character_property_map_cpp.bat
@@ -8,4 +8,4 @@ curl http://www.unicode.org/Public/13.0.0/ucd/auxiliary/GraphemeBreakProperty.tx
 
 python3 %~dp0\make_character_property_map.py %~dp0\EastAsianWidth.txt %~dp0\emoji-data.txt %~dp0\GraphemeBreakProperty.txt %~dp0\character_property_map.txt
 
-python3 %~dp0\make_character_property_map_cpp.py %~dp0\character_property_map.txt %~dp0\..\library\text\cpp\src\tetengo.text.character_property_map.cpp
+python3 %~dp0\make_character_property_map_cpp.py %~dp0\character_property_map.txt %~dp0\..\library\text\cpp\src\tetengo.text.grapheme_splitting.character_property_map.cpp
