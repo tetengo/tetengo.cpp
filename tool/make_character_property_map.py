2d0ea75b42888728ae8e08d47b236c5f0de45b57
kaoru <kaorut@mc.pikara.ne.jp>
2021-01-11 04:19:01 +0900

    Reformat the scripts #125

--- tool/make_character_property_map.py
+++ tool/make_character_property_map.py
@@ -2,25 +2,26 @@
 # Makes character_property_map.txt for tetengo::text
 # Copyright (C) 2019-2021 kaoru  https://www.tetengo.org/
 
-from pathlib import Path
 import re
 import sys
+from pathlib import Path
 
 
 def main():
     if len(sys.argv) <= 3:
         print(
             "Usage: ./make_character_property_map.py EastAsianWidth.txt emoji-data.txt GraphemeBreakProperty.txt character_property_map.txt",
-            file=sys.stderr
+            file=sys.stderr,
         )
         sys.exit(0)
-    east_asian_width_map = load_file(Path(sys.argv[1]), 'N')
-    emoji_data_map = load_file(Path(sys.argv[2]), 'N')
-    grapheme_map = load_file(Path(sys.argv[3]), 'Other')
+    east_asian_width_map = load_file(Path(sys.argv[1]), "N")
+    emoji_data_map = load_file(Path(sys.argv[2]), "N")
+    grapheme_map = load_file(Path(sys.argv[3]), "Other")
     save_file(Path(sys.argv[4]), east_asian_width_map, emoji_data_map, grapheme_map)
 
+
 def load_file(path, default_value):
-    map = [ default_value for code in range(0x110000) ]
+    map = [default_value for code in range(0x110000)]
     with path.open(mode="r", encoding="UTF-8") as stream:
         for line in stream:
             line = remove_comment(line)
@@ -38,20 +39,25 @@ def load_file(path, default_value):
                     map[code] = value
     return map
 
+
 def remove_comment(line):
     line = line.rstrip("\r\n")
     if "#" in line:
-        line = line[0:line.find("#")]
+        line = line[0 : line.find("#")]
     line = line.rstrip(" ")
     return line
 
+
 def save_file(path, east_asian_width_map, emoji_data_map, grapheme_map):
-    with path.open(mode='w', newline='\r\n', encoding="UTF-8") as stream:
-        previous_value = ''
+    with path.open(mode="w", newline="\r\n", encoding="UTF-8") as stream:
+        previous_value = ""
         for code in range(0x110000):
-            value = "{}\t{}\t{}".format(east_asian_width_map[code], emoji_data_map[code], grapheme_map[code])
+            value = "{}\t{}\t{}".format(
+                east_asian_width_map[code], emoji_data_map[code], grapheme_map[code]
+            )
             if value != previous_value:
-                stream.write("{}\t{}\n".format(format(code, '04X'), value))
+                stream.write("{}\t{}\n".format(format(code, "04X"), value))
                 previous_value = value
+
 
 main()
