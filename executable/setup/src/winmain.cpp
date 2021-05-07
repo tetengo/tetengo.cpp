6a8b875ce94dd72ddf65de4bb7ae099af4b7f39e
kaoru <kaorut@mc.pikara.ne.jp>
2021-05-07 22:55:03 +0900

    Create a setup bootstrapper #140

---
+++ executable/setup/src/winmain.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+
+int WINAPI WinMain(
+    const ::HINSTANCE /*hInstance*/,
+    const ::HINSTANCE /*hPrevInstance*/,
+    const ::LPSTR /*lpCmdLine*/,
+    const int /*nShowCmd*/)
+{
+    return 0;
+}
