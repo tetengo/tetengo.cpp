5d0e5dff6b8b3d2a4d86181cdb0990421f6d4c47
kaoru <kaorut@mc.pikara.ne.jp>
2021-05-29 20:00:42 +0900

    Revert "Call the instance() static function to get the file_storage_loader instance #115"
    This reverts commit bbec41c9d2667900c0bdec4babf8d181b1e4a6f8.

--- library/property/cpp/src/tetengo.property.file_storage.cpp
+++ library/property/cpp/src/tetengo.property.file_storage.cpp
@@ -77,15 +77,6 @@ namespace tetengo::property
     class file_storage_loader::impl : private boost::noncopyable
     {
     public:
-        // static functions
-
-        static const file_storage_loader& instance()
-        {
-            static const file_storage_loader singleton{};
-            return singleton;
-        }
-
-
         // functions
 
         std::unique_ptr<storage> load_impl(const std::filesystem::path& path) const
@@ -135,14 +126,9 @@ namespace tetengo::property
     };
 
 
-    const file_storage_loader& file_storage_loader::instance()
-    {
-        return impl::instance();
-    }
+    file_storage_loader::file_storage_loader() : m_p_impl{ std::make_unique<impl>() } {}
 
     file_storage_loader::~file_storage_loader() = default;
-
-    file_storage_loader::file_storage_loader() : m_p_impl{ std::make_unique<impl>() } {}
 
     std::unique_ptr<storage> file_storage_loader::load_impl(const std::filesystem::path& path) const
     {
