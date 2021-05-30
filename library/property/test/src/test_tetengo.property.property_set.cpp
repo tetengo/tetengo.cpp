1b4fa3e9b9a92e984b177afbf6006fa90a853c1b
kaoru <kaorut@mc.pikara.ne.jp>
2021-05-30 10:04:13 +0900

    Implement tetengo::property::property_set #115

--- library/property/test/src/test_tetengo.property.property_set.cpp
+++ library/property/test/src/test_tetengo.property.property_set.cpp
@@ -6,6 +6,7 @@
 
 #include <filesystem>
 #include <memory>
+#include <optional>
 #include <utility>
 
 #include <boost/preprocessor.hpp>
@@ -48,10 +49,11 @@ BOOST_AUTO_TEST_CASE(get_bool)
     BOOST_TEST_PASSPOINT();
 
     {
-        auto                            p_storage_loader = std::make_unique<tetengo::property::memory_storage_loader>();
-        tetengo::property::property_set property_set_{ std::move(p_storage_loader), property_set_path() };
+        auto p_storage_loader = std::make_unique<tetengo::property::memory_storage_loader>();
+        const tetengo::property::property_set property_set_{ std::move(p_storage_loader), property_set_path() };
 
-        property_set_.update();
+        const auto o_value = property_set_.get_bool("alpha");
+        BOOST_CHECK(!o_value);
     }
 }
 
@@ -63,7 +65,19 @@ BOOST_AUTO_TEST_CASE(set_bool)
         auto                            p_storage_loader = std::make_unique<tetengo::property::memory_storage_loader>();
         tetengo::property::property_set property_set_{ std::move(p_storage_loader), property_set_path() };
 
-        property_set_.update();
+        property_set_.set_bool("alpha", false);
+        const auto o_value = property_set_.get_bool("alpha");
+        BOOST_REQUIRE(o_value);
+        BOOST_TEST(!*o_value);
+    }
+    {
+        auto                            p_storage_loader = std::make_unique<tetengo::property::memory_storage_loader>();
+        tetengo::property::property_set property_set_{ std::move(p_storage_loader), property_set_path() };
+
+        property_set_.set_bool("alpha", true);
+        const auto o_value = property_set_.get_bool("alpha");
+        BOOST_REQUIRE(o_value);
+        BOOST_TEST(*o_value);
     }
 }
 
@@ -72,10 +86,11 @@ BOOST_AUTO_TEST_CASE(get_uint32)
     BOOST_TEST_PASSPOINT();
 
     {
-        auto                            p_storage_loader = std::make_unique<tetengo::property::memory_storage_loader>();
-        tetengo::property::property_set property_set_{ std::move(p_storage_loader), property_set_path() };
+        auto p_storage_loader = std::make_unique<tetengo::property::memory_storage_loader>();
+        const tetengo::property::property_set property_set_{ std::move(p_storage_loader), property_set_path() };
 
-        property_set_.update();
+        const auto o_value = property_set_.get_uint32("bravo");
+        BOOST_CHECK(!o_value);
     }
 }
 
@@ -87,7 +102,10 @@ BOOST_AUTO_TEST_CASE(set_uint32)
         auto                            p_storage_loader = std::make_unique<tetengo::property::memory_storage_loader>();
         tetengo::property::property_set property_set_{ std::move(p_storage_loader), property_set_path() };
 
-        property_set_.update();
+        property_set_.set_uint32("bravo", 42);
+        const auto o_value = property_set_.get_uint32("bravo");
+        BOOST_REQUIRE(o_value);
+        BOOST_TEST(*o_value == 42U);
     }
 }
 
@@ -96,10 +114,11 @@ BOOST_AUTO_TEST_CASE(get_string)
     BOOST_TEST_PASSPOINT();
 
     {
-        auto                            p_storage_loader = std::make_unique<tetengo::property::memory_storage_loader>();
-        tetengo::property::property_set property_set_{ std::move(p_storage_loader), property_set_path() };
+        auto p_storage_loader = std::make_unique<tetengo::property::memory_storage_loader>();
+        const tetengo::property::property_set property_set_{ std::move(p_storage_loader), property_set_path() };
 
-        property_set_.update();
+        const auto o_value = property_set_.get_string("charlie");
+        BOOST_CHECK(!o_value);
     }
 }
 
@@ -111,7 +130,10 @@ BOOST_AUTO_TEST_CASE(set_string)
         auto                            p_storage_loader = std::make_unique<tetengo::property::memory_storage_loader>();
         tetengo::property::property_set property_set_{ std::move(p_storage_loader), property_set_path() };
 
-        property_set_.update();
+        property_set_.set_string("charlie", "hoge");
+        const auto o_value = property_set_.get_string("charlie");
+        BOOST_REQUIRE(o_value);
+        BOOST_TEST(*o_value == "hoge");
     }
 }
 
