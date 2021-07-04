e270ca2ac8d605a8184ec3109fff383ac46d53fe
kaoru <kaorut@mc.pikara.ne.jp>
2021-07-04 20:52:35 +0900

    Implement the usage samples for tetengo::lattice #160

--- library/lattice/test/src/usage_tetengo.lattice.viterbi_c.c
+++ library/lattice/test/src/usage_tetengo.lattice.viterbi_c.c
@@ -7,12 +7,14 @@
 #include "usage_tetengo.lattice.viterbi_c.h"
 
 /* [viterbi] */
+#include <stdlib.h>
 #include <string.h>
 
 #include <tetengo/lattice/connection.h>
 #include <tetengo/lattice/entry.h>
 #include <tetengo/lattice/lattice.h>
 #include <tetengo/lattice/node.h>
+#include <tetengo/lattice/stringView.h>
 #include <tetengo/lattice/vocabulary.h>
 
 
@@ -48,16 +50,18 @@ void usage_tetengo_lattice_viterbi()
     tetengo_lattice_lattice_pushBack(p_lattice, "a");
     tetengo_lattice_lattice_pushBack(p_lattice, "b");
 
-    tetengo_lattice_node_t eos_node = { 0 };
     {
         const size_t eos_preceding_count = tetengo_lattice_lattice_settle(p_lattice, NULL, NULL);
         if (eos_preceding_count == 0)
         {
             return;
         }
-        int* const p_eos_preceding_costs = (int*)malloc(eos_preceding_count * sizeof(int));
-        tetengo_lattice_lattice_settle(p_lattice, &eos_node, p_eos_preceding_costs);
-        free((void*)p_eos_preceding_costs);
+        {
+            int* const             p_eos_preceding_costs = (int*)malloc(eos_preceding_count * sizeof(int));
+            tetengo_lattice_node_t eos_node;
+            tetengo_lattice_lattice_settle(p_lattice, &eos_node, p_eos_preceding_costs);
+            free((void*)p_eos_preceding_costs);
+        }
     }
 
     tetengo_lattice_lattice_destroy(p_lattice);
@@ -95,45 +99,72 @@ entry_equal_to(const tetengo_lattice_entryView_t* const p_entry1, const tetengo_
 tetengo_lattice_vocabulary_t* build_vocabulary()
 {
     const tetengo_lattice_entry_t entries[] = {
-        // clang-format off
+        /* clang-format off */
         { { "a", 1 }, "Alpha", 2 },
         { { "a", 1 }, "Alice", 1 },
         { { "b", 1 }, "Bravo", 7 },
         { { "b", 1 }, "Bob", 8 },
         { { "ab", 2 }, "AwaBizan", 9 },
-        // clang-format on
+        /* clang-format on */
     };
-    const tetengo_lattice_entry_t          bosEos = { tetengo_lattice_entry_bosEos()->key,
-                                             tetengo_lattice_entry_valueOf(
-                                                 tetengo_lattice_entry_bosEos()->value_handle),
-                                             tetengo_lattice_entry_bosEos()->cost };
-    const tetengo_lattice_keyEntriesPair_t entry_mappings[] = {
-        // clang-format off
-        { { "a", 1 }, &entries[0], 2 },
-        { { "b", 1 }, &entries[2], 2 },
-        { { "ab", 2 }, &entries[4], 1 },
-        // clang-format on
-    };
-    const tetengo_lattice_entriesConnectionCostPair_t connections[] = {
-        // clang-format off
-        { &bosEos, &entries[0], 3 },
-        { &bosEos, &entries[1], 1 },
-        { &entries[0], &entries[2], 4 },
-        { &entries[1], &entries[2], 1 },
-        { &entries[0], &entries[3], 5 },
-        { &entries[1], &entries[3], 9 },
-        { &entries[2], &bosEos, 2 },
-        { &entries[3], &bosEos, 6 },
-        { &bosEos, &entries[4], 7 },
-        { &entries[4], &bosEos, 1 },
-        // clang-format on
-    };
-    return tetengo_lattice_vocabulary_createUnorderedMapVocabulary(
-        entry_mappings,
-        sizeof(entry_mappings) / sizeof(tetengo_lattice_keyEntriesPair_t),
-        connections,
-        sizeof(connections) / sizeof(tetengo_lattice_entriesConnectionCostPair_t),
-        entry_hash,
-        entry_equal_to);
+    tetengo_lattice_entry_t bosEos;
+    bosEos.key = tetengo_lattice_entry_bosEos()->key;
+    bosEos.p_value = tetengo_lattice_entry_valueOf(tetengo_lattice_entry_bosEos()->value_handle);
+    bosEos.cost = tetengo_lattice_entry_bosEos()->cost;
+    {
+        tetengo_lattice_keyEntriesPair_t entry_mappings[3];
+        entry_mappings[0].key.p_head = "a";
+        entry_mappings[0].key.length = 1;
+        entry_mappings[0].p_entries = &entries[0];
+        entry_mappings[0].entry_count = 2;
+        entry_mappings[1].key.p_head = "b";
+        entry_mappings[1].key.length = 1;
+        entry_mappings[1].p_entries = &entries[2];
+        entry_mappings[1].entry_count = 2;
+        entry_mappings[2].key.p_head = "ab";
+        entry_mappings[2].key.length = 2;
+        entry_mappings[2].p_entries = &entries[4];
+        entry_mappings[2].entry_count = 1;
+        {
+            tetengo_lattice_entriesConnectionCostPair_t connections[10];
+            connections[0].p_from = &bosEos;
+            connections[0].p_to = &entries[0];
+            connections[0].cost = 3;
+            connections[1].p_from = &bosEos;
+            connections[1].p_to = &entries[1];
+            connections[1].cost = 1;
+            connections[2].p_from = &entries[0];
+            connections[2].p_to = &entries[2];
+            connections[2].cost = 4;
+            connections[3].p_from = &entries[1];
+            connections[3].p_to = &entries[2];
+            connections[3].cost = 1;
+            connections[4].p_from = &entries[0];
+            connections[4].p_to = &entries[3];
+            connections[4].cost = 5;
+            connections[5].p_from = &entries[1];
+            connections[5].p_to = &entries[3];
+            connections[5].cost = 9;
+            connections[6].p_from = &entries[2];
+            connections[6].p_to = &bosEos;
+            connections[6].cost = 2;
+            connections[7].p_from = &entries[3];
+            connections[7].p_to = &bosEos;
+            connections[7].cost = 6;
+            connections[8].p_from = &bosEos;
+            connections[8].p_to = &entries[4];
+            connections[8].cost = 7;
+            connections[9].p_from = &entries[4];
+            connections[9].p_to = &bosEos;
+            connections[9].cost = 1;
+            return tetengo_lattice_vocabulary_createUnorderedMapVocabulary(
+                entry_mappings,
+                sizeof(entry_mappings) / sizeof(tetengo_lattice_keyEntriesPair_t),
+                connections,
+                sizeof(connections) / sizeof(tetengo_lattice_entriesConnectionCostPair_t),
+                entry_hash,
+                entry_equal_to);
+        }
+    }
 }
 /* [viterbi] */
