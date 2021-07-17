c83183989356e4badf81c8edcb5bd3894ba40a35
kaoru <kaorut@mc.pikara.ne.jp>
2021-07-17 16:29:05 +0900

    Create usage samples for tetengo::text::grapheme_splitter #160

---
+++ library/text/test/src/usage_tetengo.text.graphemeSplit_c.c
@@ -0,0 +1,11 @@
+/*! \file
+    \brief The usage of grapheme splitting.
+
+    Copyright (C) 2019-2021 kaoru  https://www.tetengo.org/
+*/
+
+#include "usage_tetengo.text.graphemeSplit_c.h"
+
+/* [grapheme_split] */
+void usage_tetengo_text_graphemeSplit() {}
+/* [grapheme_split] */
---
+++ library/text/test/src/usage_tetengo.text.grapheme_split_cpp.cpp
@@ -0,0 +1,16 @@
+/*! \file
+    \brief The usage of grapheme splitting.
+
+    Copyright (C) 2019-2021 kaoru  https://www.tetengo.org/
+*/
+
+#include "usage_tetengo.text.grapheme_split_cpp.hpp"
+
+// [grapheme_split]
+
+
+namespace usage_tetengo::text
+{
+    void grapheme_split() {}
+}
+// [grapheme_split]
