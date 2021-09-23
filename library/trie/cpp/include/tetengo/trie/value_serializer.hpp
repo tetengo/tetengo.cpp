73754958ba753c64103782c45ae8b43738d82fde
kaoru <kaorut@mc.pikara.ne.jp>
2021-09-23 13:38:29 +0900

    Create tetengo::trie::value_serializer and value_deserializer #192

---
+++ library/trie/cpp/include/tetengo/trie/value_serializer.hpp
@@ -0,0 +1,54 @@
+/*! \file
+    \brief A value serializer.
+
+    Copyright (C) 2019-2021 kaoru  https://www.tetengo.org/
+*/
+
+#if !defined(TETENGO_TRIE_VALUESERIALIZER_HPP)
+#define TETENGO_TRIE_VALUESERIALIZER_HPP
+
+
+namespace tetengo::trie
+{
+    /*!
+        \brief A value serializer.
+    */
+    class value_serializer
+    {
+    public:
+        // constructors and destructor
+
+        /*!
+            \brief Creates a value serializer.
+        */
+        value_serializer();
+
+
+    private:
+        // variables
+    };
+
+
+    /*!
+        \brief A value deserializer.
+    */
+    class value_deserializer
+    {
+    public:
+        // constructors and destructor
+
+        /*!
+            \brief Creates a value deserializer.
+        */
+        value_deserializer();
+
+
+    private:
+        // variables
+    };
+
+
+}
+
+
+#endif
---
+++ library/trie/cpp/src/tetengo.trie.value_serializer.cpp
@@ -0,0 +1,18 @@
+/*! \file
+    \brief A value serializer.
+
+    Copyright (C) 2019-2021 kaoru  https://www.tetengo.org/
+*/
+
+#include <tetengo/trie/value_serializer.hpp>
+
+
+namespace tetengo::trie
+{
+    value_serializer::value_serializer() {}
+
+
+    value_deserializer::value_deserializer() {}
+
+
+}
