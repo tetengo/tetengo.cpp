fe80f35ba7cf0128ea2b7b7dc9d8ea0a4e99258f
kaoru <kaorut@mc.pikara.ne.jp>
2021-11-28 21:53:55 +0900

    Create a test suite for the custom vocabulary #207

---
+++ library/lattice/test/src/test_tetengo.lattice.custom_vocabulary.cpp
@@ -0,0 +1,40 @@
+/*! \file
+    \brief A custom vocabulary.
+
+    Copyright (C) 2019-2021 kaoru  https://www.tetengo.org/
+*/
+
+#include <boost/preprocessor.hpp>
+#include <boost/test/unit_test.hpp>
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo)
+BOOST_AUTO_TEST_SUITE(lattice)
+BOOST_AUTO_TEST_SUITE(custom_vocabulary)
+
+
+BOOST_AUTO_TEST_CASE(construction)
+{
+    BOOST_TEST_PASSPOINT();
+
+    BOOST_WARN_MESSAGE(false, "Implemtn it.");
+}
+
+BOOST_AUTO_TEST_CASE(find_entries)
+{
+    BOOST_TEST_PASSPOINT();
+
+    BOOST_WARN_MESSAGE(false, "Implemtn it.");
+}
+
+BOOST_AUTO_TEST_CASE(find_connection)
+{
+    BOOST_TEST_PASSPOINT();
+
+    BOOST_WARN_MESSAGE(false, "Implemtn it.");
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
