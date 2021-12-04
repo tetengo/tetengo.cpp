b25c2addd3a7faecf9d0c10ed9660b6ce16f3117
kaoru <kaorut@mc.pikara.ne.jp>
2021-12-04 11:26:41 +0900

    Create a test suite for the custom vocabulary #206

---
+++ library/lattice/test/src/test_tetengo.lattice.custom_input.cpp
@@ -0,0 +1,58 @@
+/*! \file
+    \brief A custom input.
+
+    Copyright (C) 2019-2021 kaoru  https://www.tetengo.org/
+*/
+
+#include <boost/preprocessor.hpp>
+#include <boost/test/unit_test.hpp>
+
+
+namespace
+{}
+
+
+BOOST_AUTO_TEST_SUITE(test_tetengo)
+BOOST_AUTO_TEST_SUITE(lattice)
+BOOST_AUTO_TEST_SUITE(custom_input)
+
+
+BOOST_AUTO_TEST_CASE(construction)
+{
+    BOOST_TEST_PASSPOINT();
+
+    BOOST_WARN_MESSAGE(false, "Implement it.");
+}
+
+BOOST_AUTO_TEST_CASE(value)
+{
+    BOOST_TEST_PASSPOINT();
+
+    BOOST_WARN_MESSAGE(false, "Implement it.");
+}
+
+BOOST_AUTO_TEST_CASE(length)
+{
+    BOOST_TEST_PASSPOINT();
+
+    BOOST_WARN_MESSAGE(false, "Implement it.");
+}
+
+BOOST_AUTO_TEST_CASE(create_subrange)
+{
+    BOOST_TEST_PASSPOINT();
+
+    BOOST_WARN_MESSAGE(false, "Implement it.");
+}
+
+BOOST_AUTO_TEST_CASE(append)
+{
+    BOOST_TEST_PASSPOINT();
+
+    BOOST_WARN_MESSAGE(false, "Implement it.");
+}
+
+
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END()
