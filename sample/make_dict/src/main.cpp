6d6d23088b69053d45afa7dfaa36f89bc44e9c8a
kaoru <kaorut@mc.pikara.ne.jp>
2023-10-01 15:22:40 +0900

    Format the code #301

--- sample/make_dict/src/main.cpp
+++ sample/make_dict/src/main.cpp
@@ -202,8 +202,7 @@ namespace
     build_trie(const std::unordered_map<std::string, std::vector<std::pair<std::size_t, std::size_t>>>& word_offset_map)
     {
         std::cerr << "Building trie..." << std::endl;
-        std::vector<std::pair<std::string, std::vector<std::pair<std::size_t, std::size_t>>>> word_offset_vector
-        {
+        std::vector<std::pair<std::string, std::vector<std::pair<std::size_t, std::size_t>>>> word_offset_vector{
             std::begin(word_offset_map), std::end(word_offset_map)
         };
         std::sort(std::begin(word_offset_vector), std::end(word_offset_vector));
